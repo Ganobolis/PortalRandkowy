9ea81a456ea17e35e1a31e68588f759c0b42041b
Bartosz Konikowski <bartosz.konikowski@ihsmarkit.com>
2020-08-19 14:25:00 +0200

    Create repositry for web api

---
+++ PortalRandkowy.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Controllers
+{
+    //GET http://localhost:5000/WeatherForecast
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public WeatherForecastController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        //WeatherForecast
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        //WeatherForecast/10
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        //WeatherForecast
+        [HttpPost()]
+        public async Task<IActionResult> AddValue([FromBody] Value value)
+        {
+            _context.Values.Add(value);
+            await _context.SaveChangesAsync();
+            return Ok(value);
+        }
+
+        //WeatherForecast/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditValue(int id, [FromBody] Value value)
+        {
+            var data = await _context.Values.FindAsync(id);
+            data.Name = value.Name;
+            _context.Values.Update(data);
+            await _context.SaveChangesAsync();
+            return Ok(data);
+        }
+
+        //WeatherForecast/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteValue(int id)
+        {
+            var  data = await _context.Values.FindAsync(id);
+            _context.Values.Remove(data);
+            await _context.SaveChangesAsync();
+            return Ok(data);
+        }
+    }
+}
